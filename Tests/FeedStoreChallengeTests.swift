eb1fcd3eee53d06cb01ae37744c5ff04d654cf93
Miguel Duran <miguel.duran.romero@concrete.com.br>
2020-06-03 18:27:12 -0400

    Add implementation to test when SUT delivers empty on empty cache.

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -14,9 +14,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 //
 
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-//		let sut = makeSUT()
-//
-//		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
 	}
 
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -87,8 +87,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	
 	// - MARK: Helpers
 	
-	private func makeSUT() -> FeedStore {
-		fatalError("Must be implemented")
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+		let sut = CoreDataFeedStore()
+        return sut
 	}
 	
 }
