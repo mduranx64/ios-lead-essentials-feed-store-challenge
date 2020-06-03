b2679dad67fc5a8270f4ac6309a2d37f033605a1
Miguel Duran <miguel.duran.romero@concrete.com.br>
2020-06-03 18:10:28 -0400

    Create `CoreDataFeedStore` class that implements `FeedStore` protocol with empty methods.

---
+++ FeedStoreChallenge/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Miguel Duran on 03-06-20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public class CoreDataFeedStore: FeedStore {
+    
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        
+    }
+}
