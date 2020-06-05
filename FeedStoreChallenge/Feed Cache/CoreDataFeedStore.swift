4d912e6af2052a30dd00c14fba15fd8829086575
Miguel Duran <miguel.duran.romero@concrete.com.br>
2020-06-05 00:11:54 -0400

    Remove unused code and move CoreData NSManagedObjects inside `CoreDataFeedStore` so they are accesible in that scope only.

--- FeedStoreChallenge/Feed Cache/CoreDataFeedStore.swift
+++ FeedStoreChallenge/Feed Cache/CoreDataFeedStore.swift
@@ -11,6 +11,62 @@ import CoreData
 
 public class CoreDataFeedStore: FeedStore {
     
+    @objc(Cache)
+    private class Cache: NSManagedObject {
+        @nonobjc internal class func createFetchRequest() -> NSFetchRequest<Cache> {
+            return NSFetchRequest<Cache>(entityName: "Cache")
+        }
+
+        @NSManaged internal var timestamp: Date
+        @NSManaged internal var feed: NSOrderedSet
+        
+        convenience init(context moc: NSManagedObjectContext, feed: [CoreDataFeedImage], timestamp: Date) {
+            let name = String(describing: type(of: self))
+            guard let entity = NSEntityDescription.entity(forEntityName: name, in: moc) else {
+                fatalError("There is no entity with name \(name)")
+            }
+            self.init(entity: entity, insertInto: moc)
+            self.timestamp = timestamp
+            self.feed = NSOrderedSet(array: feed)
+        }
+        
+        var localFeed: [LocalFeedImage] {
+            let images = self.feed.array as? [CoreDataFeedImage] ?? []
+            return images.map { $0.local }
+        }
+    }
+    
+    @objc(CoreDataFeedImage)
+    private class CoreDataFeedImage: NSManagedObject {
+
+        @NSManaged internal var feedDescription: String?
+        @NSManaged internal var id: UUID
+        @NSManaged internal var location: String?
+        @NSManaged internal var url: URL
+        @NSManaged internal var cache: Cache
+        
+        convenience init(context moc: NSManagedObjectContext, image: LocalFeedImage) {
+            let name = String(describing: type(of: self))
+            guard let entity = NSEntityDescription.entity(forEntityName: name, in: moc) else {
+                fatalError("There is no entity with name \(name)")
+            }
+            self.init(entity: entity, insertInto: moc)
+            self.id = image.id
+            self.location = image.location
+            self.feedDescription = image.description
+            self.url = image.url
+        }
+        
+        var local: LocalFeedImage {
+            return LocalFeedImage(
+                id: id,
+                description: feedDescription,
+                location: location,
+                url: url
+            )
+        }
+    }
+    
     private let storeURL: URL
 
     public init(storeURL: URL) {
