2c029fb9d964e7396d96feceb5532fdb4c9423db
Miguel Duran <miguel.duran.romero@concrete.com.br>
2020-06-11 20:26:37 -0400

    Create Integration tests file.

---
+++ Tests/FeedStoreChallengeIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedStoreChallengeIntegrationTests.swift
+//  Tests
+//
+//  Created by Miguel Duran on 11-06-20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+class FeedStoreChallengeIntegrationTests: XCTestCase {
+
+
+}
